58c361000067e785f667b4c4589729459b03f1b1
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-13 15:35:58 -0700

    football scores

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // import functions and grab DOM elements
+const ninersScoreEl = document.querySelector('#niners-score')
+const seahawksScoreEl = document.querySelector('#seahawks-score')
+const ninersButtonEl = document.querySelector('#niners-button')
+const seahawksButtonEl = document.querySelector('#seahawks-button')
+
 
 // initialize state
-
+let ninersScore = 0;
+let seahawksScore = 0;
 // set event listeners 
+ninersButtonEl.addEventListener('click', () => {
+  ninersScore += 7;
+  ninersScoreEl.textContent = ninersScore;
+})
+
+seahawksButtonEl.addEventListener('click', () => {
+  seahawksScore += 7;
+  seahawksScoreEl.textContent = seahawksScore;
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
